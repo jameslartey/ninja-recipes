aebda5d7de03d276a680555e3feb02f7ce074043
mosesgameli <69321992+mosesgameli@users.noreply.github.com>
2024-09-27 13:11:15 +0000

    implement recipes api route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,69 +1,66 @@
-import { 
-  Card, 
-  CardContent, 
-  CardDescription, 
-  CardFooter, 
-  CardHeader, 
-  CardTitle 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
 } from '@/components/ui/card'
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
-
-
-
-
 interface Recipe {
   title: string,
   image: string,
   time: number,
-  description:string,
+  description: string,
   vegan: boolean,
   id: string,
 
 }
 
- async function getRecipies(): Promise<Recipe[]>{
-  const result = await fetch('http://localhost:4000/recipes')
+async function getRecipies(): Promise<Recipe[]> {
+  const result = await fetch('http://localhost:3000/recipes')
 
-  await new Promise((resolve) =>setTimeout(resolve,3000))
+  await new Promise((resolve) => setTimeout(resolve, 450))
 
   return result.json()
 }
+
 export default async function Home() {
   const recipes = await getRecipies()
-  
+
   return (
-   <main>
-    <div className="grid grid-cols-3 gap-8">
-      {recipes.map(recipe => (
-     <Card key={recipe.id} className="flex flex-col justify-between">
-     <CardHeader className="flex-row gap-4 items-center">
-       <Avatar>
-        <AvatarImage src={`/img/${recipe.image}`}alt="recipe img"/>
-        <AvatarFallback>
-          {recipe.title.slice(0,2)}
-        </AvatarFallback>
-        
-       </Avatar>
-       <div>
-         <CardTitle>{recipe.title}</CardTitle>
-         <CardDescription>{recipe.time} mins to cook.</CardDescription>
-       </div>
-     </CardHeader>
-     <CardContent>
-       <p>{recipe.description}</p>
-     </CardContent>
-     <CardFooter className="flex justify-between">
-       <Button>View Recipe</Button>
-       {recipe.vegan && <Badge variant={"secondary"} >Vegan!</Badge>}
-     </CardFooter>  
-   </Card>
-  
-  ) )} 
-      
-    </div>
-   </main>
+    <main>
+      <div className="grid grid-cols-3 gap-8">
+        {recipes.map(recipe => (
+          <Card key={recipe.id} className="flex flex-col justify-between">
+            <CardHeader className="flex-row gap-4 items-center">
+              <Avatar>
+                <AvatarImage src={`/img/${recipe.image}`} alt="recipe img" />
+                <AvatarFallback>
+                  {recipe.title.slice(0, 2)}
+                </AvatarFallback>
+
+              </Avatar>
+              <div>
+                <CardTitle>{recipe.title}</CardTitle>
+                <CardDescription>{recipe.time} mins to cook.</CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <p>{recipe.description}</p>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <Button>View Recipe</Button>
+              {recipe.vegan && <Badge variant={"secondary"} >Vegan!</Badge>}
+            </CardFooter>
+          </Card>
+
+        ))}
+
+      </div>
+    </main>
   )
 }
